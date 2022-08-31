34bfb350e73d31943020598fd6ee5da6da1001a6
Alhaytham Attia <aattia@ntgclarity.com>
2022-08-31 15:32:36 +0300

    start a new activity when clicking a button. check if username is empty.

--- app/src/main/java/com/ntgclarity/basicapp/MainActivity.kt
+++ app/src/main/java/com/ntgclarity/basicapp/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.ntgclarity.basicapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    var etUsername: EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etUsername = findViewById(R.id.et_username)
+
+        val btnLogin = findViewById<Button>(R.id.btn_login)
+        val btnRegister = findViewById<Button>(R.id.btn_register)
+
+        btnLogin.setOnClickListener(this)
+        btnRegister.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View?) {
+        if (view?.id == R.id.btn_login) {
+            val username = etUsername?.text
+
+            if (TextUtils.isEmpty(username)) {
+                Toast.makeText(this, "Username cannot be empty!", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(this, "Well done!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        } else if (view?.id == R.id.btn_register) {
+            val intent = Intent(this, RegistrationActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
