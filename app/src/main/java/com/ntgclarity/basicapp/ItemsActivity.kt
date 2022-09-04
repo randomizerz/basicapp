27c45c3d5b3702f9ced806b14b6185ae499765e6
Alhaytham Attia <aattia@ntgclarity.com>
2022-09-04 16:02:10 +0300

    use glide to load an image from internet.

--- app/src/main/java/com/ntgclarity/basicapp/ItemsActivity.kt
+++ app/src/main/java/com/ntgclarity/basicapp/ItemsActivity.kt
@@ -5,38 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
 class ItemsActivity : AppCompatActivity() {
+    private var adapter: ItemsAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items)
 
         val rvItems = findViewById<RecyclerView>(R.id.rv_items)
 
-        rvItems.adapter = ItemsAdapter(
+        adapter = ItemsAdapter(
             listOf(
-                "abc",
-                "zxc",
-                "abc",
-                "zxc",
-                "abc",
-                "zxc",
-                "abc",
-                "zxc",
-                "abc",
-                "abc",
-                "abc",
-                "abc", "abc",
-                "abc",
-                "abc",
-                "abc",
-                "abc",
-                "abc",
-                "abc",
-                "abc",
-                "abc",
-                "abc",
-                "abc",
-                "abc"
+                "https://picsum.photos/200/300",
+                "https://picsum.photos/200/310"
             )
         )
+        rvItems.adapter = adapter
     }
+
+//    override fun onResume() {
+//        super.onResume()
+//
+//        adapter?.items = listOf(
+//            "https://picsum.photos/200/200",
+//            "https://picsum.photos/200/210",
+//            "https://picsum.photos/300/210"
+//        )
+//    }
 }
--- app/src/main/java/com/ntgclarity/basicapp/ItemsAdapter.kt
+++ app/src/main/java/com/ntgclarity/basicapp/ItemsAdapter.kt
@@ -6,15 +6,28 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-class ItemsAdapter(val items: List<String>) : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>() {
-    class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+class ItemsAdapter(var items: List<String>) : RecyclerView.Adapter<ItemsAdapter.ItemViewHolder>() {
+    inner class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var ivPhoto: ImageView? = null
         var tvTitle: TextView? = null
 
         init {
             ivPhoto = view.findViewById(R.id.iv_item)
             tvTitle = view.findViewById(R.id.tv_item)
+
+            itemView.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(view: View?) {
+                    items = listOf(
+                        "https://picsum.photos/200/200",
+                        "https://picsum.photos/200/210",
+                        "https://picsum.photos/300/210"
+                    )
+
+                    notifyDataSetChanged()
+                }
+            })
         }
     }
 
@@ -26,6 +39,10 @@ class ItemsAdapter(val items: List<String>) : RecyclerView.Adapter<ItemsAdapter.
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        Glide.with(holder.itemView)
+            .load(items[position])
+            .into(holder.ivPhoto!!)
+
         holder.tvTitle?.text = items[position]
     }
 
